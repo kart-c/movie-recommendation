f1b4f72b2b78148f5c0d6cd8f002e6e18c454796
Kartikeya Choudhary <89142612+kart-c@users.noreply.github.com>
2021-08-23 12:37:52 +0530

    chore made a movies list to display when clicked on buttons

--- src/App.js
+++ src/App.js
@@ -7,19 +7,79 @@ const moviesList = {
       movieName: "The usual suspects",
       description:
         "Five criminals meet during a routine police line-up. Upon their release, they plan to pull off a dangerous heist involving precious emeralds worth three million dollars.",
-      rating: 8.5
+      rating: 4.5
     },
     {
       movieName: "Se7en",
       description:
         "A serial killer begins murdering people according to the seven deadly sins. Two detectives, one new to the city and the other about to retire, are tasked with apprehending the criminal.",
-      rating: 8.6
+      rating: 4.6
     },
     {
       movieName: "Nightcrawler",
       description:
         "Louis Bloom, a petty thief, realises that he can make money by capturing photographs of crime scenes and starts resorting to extreme tactics to get them.",
-      rating: 8.2
+      rating: 4.5
+    }
+  ],
+  action: [
+    {
+      movieName: "Taken",
+      description:
+        "An ex-Secret Service agent's teenage daughter is abducted by human traffickers while on a trip to Paris. With almost no information on her whereabouts, he travels to Paris to find and save her.",
+      rating: 4.4
+    },
+    {
+      movieName: "John Wick",
+      description:
+        "The story focuses on John Wick (Reeves) searching for the men who broke into his home, stole his vintage car and killed his puppy, which was a last gift to him from his recently deceased wife.",
+      rating: 4.6
+    },
+    {
+      movieName: "The Dark Knight",
+      description:
+        "After Gordon, Dent and Batman begin an assault on Gotham's organised crime, the mobs hire the Joker, a psychopathic criminal mastermind who offers to kill Batman and bring the city to its knees.",
+      rating: 4.8
+    }
+  ],
+  fantasy: [
+    {
+      movieName: "The Lord of the Rings",
+      description:
+        "The Lord of the Rings is a series of three epic fantasy adventure films directed by Peter Jackson, based on the novel written by J. R. R. Tolkien. The films are subtitled The Fellowship of the Ring, The Two Towers, and The Return of the King.",
+      rating: 5
+    },
+    {
+      movieName: "Pan’s Labyrinth",
+      description:
+        "Ofelia moves with her mother to her stepfather's house. At night, a fairy leads her to a faun who informs her that she is a princess and she needs to participate in three tasks to prove her royalty.",
+      rating: 4.7
+    },
+    {
+      movieName: "Hugo",
+      description:
+        "Hugo is a young orphan who loves pottering around with the station clocks and whose most treasured possession is his late father's automaton. His mission is to find a key that will get it working.",
+      rating: 4.5
+    }
+  ],
+  comedy: [
+    {
+      movieName: "Hunt for the Wilderpeople",
+      description:
+        "A boy and his foster father become the subjects of a manhunt after they get stranded in the New Zealand wilderness.",
+      rating: 4.7
+    },
+    {
+      movieName: "Groundhog Day",
+      description:
+        "Phil, a self-centred weatherman, goes to the town of Punxsutawney for an assignment. He is later shocked when he wakes up the next morning and realises that he is reliving the same day over and over.",
+      rating: 4.5
+    },
+    {
+      movieName: "Rush Hour",
+      description:
+        "When a Chinese consul's young daughter is kidnapped, Hong Kong Detective Lee must team up with Carter, a loud-mouthed LA detective. Distinctive work styles and cultural differences pose hiccups.",
+      rating: 4.6
     }
   ]
 };
@@ -33,7 +93,7 @@ export default function App() {
         <div>
           <div>{selectedMovie.movieName}</div>
           <div>{selectedMovie.description}</div>
-          <div>{selectedMovie.rating}</div>
+          <div>Rating: {selectedMovie.rating}/5</div>
         </div>
       );
     });
@@ -44,9 +104,9 @@ export default function App() {
       <h1>Good Movies</h1>
       <p>Check out my favorite selection of movies.</p>
       <button onClick={onClickHandler}>Thriller</button>
-      <button onClick={onClickHandler}>sci-fi</button>
+      <button onClick={onClickHandler}>Action</button>
       <button onClick={onClickHandler}>Fantasy</button>
-      <button onClick={onClickHandler}>Romantic</button>
+      <button onClick={onClickHandler}>Comedy</button>
       <div>{movieGenre}</div>
     </div>
   );
